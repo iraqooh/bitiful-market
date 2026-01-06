2ac33888e4a234f26e1eb7402af0bba7914abf93
Harry Iraku <iraqooh@gmail.com>
2026-01-06 10:30:51 +0300

    Fixed route pattern

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use("/api/v1/products", productRouter)
 const __dirname = path.resolve()
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "/frontend/dist")))
-    app.get("/.*/", (req, res) => {
+    app.get(/.*/, (req, res) => {
         res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
     })
 }
